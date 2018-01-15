20373bbd57a1dce005cc1b8a56905483132761b8
taira120 <taira1999120@gmail.com>
2018-01-15 21:25:51 +0900

    device導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "exams#index"
+
   namespace :front, path: "/" do
     get "/app(/*other)", to: "app#index"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2018_01_11_064322) do
 
   create_table "exams", force: :cascade do |t|
     t.string "images", null: false
-    t.integer "type"
+    t.integer "type", null: false
     t.integer "grade"
     t.integer "year"
     t.datetime "created_at", null: false
@@ -55,6 +55,23 @@ ActiveRecord::Schema.define(version: 2018_01_11_064322) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
